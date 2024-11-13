75447a249b630e4ff18daa3adc4ee7b02d4019e1
suptyislam503 <kotaliparaulahati@gmail.com>
2024-11-13 10:48:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function myname(){
+    document.getElementById("myname").innerHTML ="WEB DEVELOPER"
+}
+function myimage() {
+   document.getElementById('myimage').src='image/red-flower.jpg'
+  }
+
+function image(){
+    document.getElementById('myimage').src='image/pink-flower.jpg'
+}
+
+function fontstyle(){
+   document.getElementById('fontstyle').style.fontSize="50px";
+}
+
+function hidetext(){
+    document.getElementById('hide').style.display='none'
+}
+function blockelement(){
+    document.getElementById('block-element').style.display='block'
+}
